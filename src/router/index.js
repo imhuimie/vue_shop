f830d7f9b9e49f4bf67a234cd8f48ca7307be99c
imhuimie <wuaiyw@qq.com>
2024-12-30 14:39:45 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,70 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/shopping/list',
+    meta: {
+      title: '商品列表',
+    },
+    component: (resolve) => require(['../views/shopping/list.vue'], resolve),
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: '/shopping/product/:id',
+    meta: {
+      title: '商品详情',
+    },
+    component: (resolve) => require(['../views/shopping/product.vue'], resolve),
+  },
+  {
+    path: '/shopping/cart',
+    meta: {
+      title: '购物车',
+    },
+    component: (resolve) => require(['../views/shopping/cart.vue'], resolve),
+  },
+  {
+    path: '/shopping/login/:loginStatus',
+    meta: {
+      title: '登录注册',
+    },
+    component: (resolve) => require(['../views/shopping/login.vue'], resolve),
+  },
+  {
+    path: '*',
+    redirect: '/shopping/login/login',
+  },
+
+  // 商家端
+  {
+    path: '/admin/list',
+    meta: {
+      title: '商品管理',
+    },
+    component: (resolve) => require(['../views/admin/list.vue'], resolve),
+  },
+  {
+    path: '/admin/user',
+    meta: {
+      title: '用户管理',
+    },
+    component: (resolve) => require(['../views/admin/user.vue'], resolve),
+  },
+  {
+    path: '/admin/login/:loginStatus',
+    meta: {
+      title: '登录',
+    },
+    component: (resolve) => require(['../views/admin/login.vue'], resolve),
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
