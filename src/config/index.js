cf49789f68fe203a060b513a7a64677db975353b
imhuimie <wuaiyw@qq.com>
2024-12-30 11:21:56 +0800

    环境变量

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    // 开发环境的请求地址
+    baseUrl: 'http://10.200.127.110:8080',
+    uploadFileUrl: 'http://10.200.127.110:8080/file/upload'
+  }
+   
+  export default {
+    ...CONFIG
+  }
